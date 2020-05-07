5ce1736218aa9f4f50e186c60d833f570f2cd947
Birard <roman.samidov@gmail.com>
2020-05-07 21:10:35 +0300

    1

--- src/main/java/com/company/ssl/Sender.java
+++ src/main/java/com/company/ssl/Sender.java
@@ -17,7 +17,7 @@ public class Sender {
 
         props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.port", "464");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
